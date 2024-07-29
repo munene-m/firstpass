948563f8eb326b9405b8d1c42683324f92f6b3f1
munene-m <munenenjue18@gmail.com>
2024-07-29 21:38:02 +0300

    chore: handle error when characters are not passed and when GeneratePassword is called

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/munene-m/firstpass/internal/utils"
 )
 
-func main(){
+func main() {
 	length := flag.Int("l", 15, "Length of the password")
 	useUpper := flag.Bool("U", false, "Use uppercase characters")
 	useLower := flag.Bool("L", false, "Include lowercase characters")
@@ -20,10 +20,17 @@ func main(){
 
 	flag.Parse()
 
+	if !*useUpper && !*useLower && !*useNumbers && !*useSpecial {
+		log.Fatalf("Failed to generate password: no character sets selected")
+	}
+
 	var passwords []string
 
 	for i := 0; i < *count; i++ {
-		password := utils.GeneratePassword(*length, *useUpper, *useLower, *useSpecial, *useNumbers)
+		password, err := utils.GeneratePassword(*length, *useUpper, *useLower, *useSpecial, *useNumbers)
+		if err != nil {
+			log.Fatalf("Failed to generate password: %v", err)
+		}
 		passwords = append(passwords, password)
 	}
 
@@ -36,9 +43,7 @@ func main(){
 		fmt.Println("Password(s) copied to clipboard.")
 	} else {
 		for _, password := range passwords {
-			fmt.Printf("Generated password: %s\n ", password)
+			fmt.Printf("Generated password: %s\n", password)
 		}
 	}
-	
 }
-
